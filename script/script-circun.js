243e4ae78bbaf06decd80e3307650ff7c07807a8
José Sbaraini <josesbarainips@gmail.com>
2024-11-14 17:42:49 -0400

    coisas e zas

--- script/script-circun.js
+++ script/script-circun.js
@@ -1,13 +1,27 @@
-var circuntr = document.querySelector(".tr-circun");
-var circunobj = document.getElementById('circun-line');
-var cirifo = document.getElementById("tr-cirinfo");
-circunobj.addEventListener('click', function(){
-    circuntr.classList.toggle('circun-tr-cima');
-    if (cirifo.style.height == '21vh') {
-        cirifo.style.height = '0vw'
+var circuntr1 = document.querySelector(".tr-circun-1");
+var circunobj1 = document.getElementById('circun-line-1');
+var cirifo1 = document.getElementById("tr-cirinfo-1");
+circunobj1.addEventListener('click', function(){
+    circuntr1.classList.toggle('circun-tr-cima');
+    if (cirifo1.style.height == '21vh') {
+        cirifo1.style.height = '0vw'
         
     } else {
-        cirifo.style.height = '21vh'
+        cirifo1.style.height = '21vh'
+        
+    };
+});
+
+var circuntr2 = document.querySelector(".tr-circun-2");
+var circunobj2 = document.getElementById('circun-line-2');
+var cirifo2 = document.getElementById("tr-cirinfo-2");
+circunobj2.addEventListener('click', function(){
+    circuntr2.classList.toggle('circun-tr-cima');
+    if (cirifo2.style.height == '21vh') {
+        cirifo2.style.height = '0vw'
+        
+    } else {
+        cirifo2.style.height = '21vh'
         
     };
 });
