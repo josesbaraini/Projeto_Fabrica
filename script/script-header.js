40fa2fbe1891b7040ac86110219a8b8894ed083d
José Sbaraini <josesbarainips@gmail.com>
2024-11-16 12:46:04 -0400

    coisas de header

--- script/script-header.js
+++ script/script-header.js
@@ -1,10 +1,9 @@
 var altura = document.querySelector(".aba-utilidades");
 var button = document.getElementById('button-utilidades');
-var main = document.querySelector(".main-teste");
-console.log(main);
+var main = document.querySelector(".escuro");
 button.addEventListener('click', function(){
     altura.classList.toggle('aumentada');
-    main.classList.toggle('main-diminuido');
+    main.classList.toggle('escuro-aparecendo');
 
     console.log('oi');
 });
